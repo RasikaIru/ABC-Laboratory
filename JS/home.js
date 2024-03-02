9f2c1a486d920eb5e6df3a45a772f4217df0f595
RasikaIru <rasika.iru@gmail.com>
2024-03-02 14:41:46 +0530

    Home page v1

---
+++ JS/home.js
@@ -0,0 +1,10 @@
+ src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"
+    src="JS/jquery.counterup.min.js"
+
+  
+        jQuery(document).ready(function($) {
+            $('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
